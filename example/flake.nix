fd56383f2d2f00731a9193881ee066da90350c35
Plopmenz <plopmenz@gmail.com>
2025-08-06 10:56:01 +0200

    update example flake

--- example/flake.nix
+++ example/flake.nix
@@ -1,49 +1,29 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    xnode-nodejs-template.url = "github:Openmesh-Network/xnode-nodejs-template";
+    xnode-manager.url = "github:Openmesh-Network/xnode-manager";
+    xnode-nodejs-template.url = "github:OpenxAI-Network/xnode-nodejs-template"; # "path:..";
+    nixpkgs.follows = "xnode-nodejs-template/nixpkgs";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      xnode-nodejs-template,
-      ...
-    }:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit xnode-nodejs-template;
-        };
-        modules = [
-          (
-            { xnode-nodejs-template, ... }:
-            {
-              imports = [
-                xnode-nodejs-template.nixosModules.default
-              ];
-
-              boot.isContainer = true;
-
-              services.xnode-nodejs-template = {
-                enable = true;
-              };
-
-              networking = {
-                useHostResolvConf = nixpkgs.lib.mkForce false;
-              };
-
-              services.resolved.enable = true;
-
-              system.stateVersion = "25.05";
-            }
-          )
-        ];
+  outputs = inputs: {
+    nixosConfigurations.container = inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs;
       };
+      modules = [
+        inputs.xnode-manager.nixosModules.container
+        {
+          services.xnode-container.xnode-config = {
+            host-platform = ./xnode-config/host-platform;
+            state-version = ./xnode-config/state-version;
+            hostname = ./xnode-config/hostname;
+          };
+        }
+        inputs.xnode-nodejs-template.nixosModules.default
+        {
+          services.xnode-nodejs-template.enable = true;
+        }
+      ];
     };
+  };
 }
